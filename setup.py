8559563bd77eaed1fe91b9777cae8aa7439e1933
Tim Wentzlau <tim@elektron.fo>
2016-12-09 08:39:26 +0000

    initial project creation

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kervi-cli',
+    version='0.1',
+    packages=["kervi_cli", "kervi_cli/scripts"],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        
+    ],
+    entry_points='''
+        [console_scripts]
+        kervi=kervi_cli.scripts.kervi:cli
+    ''',
+)
