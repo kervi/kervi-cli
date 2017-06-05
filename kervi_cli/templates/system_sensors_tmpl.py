260ec85e7d2fde7cf920ce03a1f1535ddab17324
Tim Wentzlau <tim@elektron.fo>
2017-06-05 22:14:53 +0100

    update templates

--- kervi_cli/templates/system_sensors_tmpl.py
+++ kervi_cli/templates/system_sensors_tmpl.py
@@ -11,13 +11,13 @@ from kervi_devices.platforms.common.sensors.cpu_temp import CPUTempSensorDeviceD
 
 CPU_SENSOR = Sensor("CPULoadSensor","CPU", CPULoadSensorDeviceDriver())
 CPU_SENSOR.store_to_db = False
-CPU_SENSOR.link_to_dashboard("*", "sys-header")
+CPU_SENSOR.link_to_dashboard("*", "header_right")
 CPU_SENSOR.link_to_dashboard("system", "cpu", type="value", size=2, link_to_header=True)
 CPU_SENSOR.link_to_dashboard("system", "cpu", type="chart", size=2)
 
 MEM_SENSOR = Sensor("MemLoadSensor", "Memory", MemUseSensorDeviceDriver())
 MEM_SENSOR.store_to_db = False
-MEM_SENSOR.link_to_dashboard("*", "sys-header")
+MEM_SENSOR.link_to_dashboard("*", "header_right")
 MEM_SENSOR.link_to_dashboard("system", "memory", type="value", size=2, link_to_header=True)
 MEM_SENSOR.link_to_dashboard("system", "memory", type="chart", size=2)
 
@@ -28,4 +28,4 @@ DISK_SENSOR.link_to_dashboard("system", "disk", type="vertical_linear_gauge", si
 #build in sensor that measures cpu temperature
 SENSOR_CPU_TEMP = Sensor("CPUTempSensor", "CPU T", CPUTempSensorDeviceDriver())
 #link to sys area top right
-SENSOR_CPU_TEMP.link_to_dashboard("*", "sys-header")
+SENSOR_CPU_TEMP.link_to_dashboard("*", "header_right")
