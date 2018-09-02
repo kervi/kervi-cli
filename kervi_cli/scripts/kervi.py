8579c784e8de3378a2ae9bd843903a7013361cdb
Tim Wentzlau <tim@wentzlau.dk>
2018-09-02 21:33:39 +0100

    Initial .io commands

--- kervi_cli/scripts/kervi.py
+++ kervi_cli/scripts/kervi.py
@@ -1,6 +1,7 @@
 import click
 from .commands.create import create
 from .commands.users import users
+from .commands.io import io
 
 @click.group()
 def cli():
@@ -8,4 +9,5 @@ def cli():
 
 cli.add_command(create)
 cli.add_command(users)
+cli.add_command(io)
 #entry_point.add_command(group2.version)    
